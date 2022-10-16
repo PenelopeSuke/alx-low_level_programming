a2f9ca751adf58b279b05602fb5496912ed6c4af
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-16 12:01:40 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,7 +9,7 @@ int main(void)
 
 	while ((h <= '7') || (t <= '8') || (u <= '9'))
 	{
-		printf ("%c%c%c, ", h, t, u);
+		printf ("%c%c%c ", h, t, u);
 		u != '9' ? ++u : (t != '8' ? (++t, u = t + 1) : (++h, t = h + 1, u = t + 1));
 	}
 	return (0);
