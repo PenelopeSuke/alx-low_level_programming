ec559ba31bd2301aa64772e86930828c256279c1
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-16 14:29:10 -0700

    t1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
  * main - prints all possible different combinations of three digits.
  * Return: Always 0
  */
-int main(void)
+int main()
 {
 	int h = '0', t = '1', u = '2';
 	while ((h <= '7') || (t <= '8') || (u <= '9'))
