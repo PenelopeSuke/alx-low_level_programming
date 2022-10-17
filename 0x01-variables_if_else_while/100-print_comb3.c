538a108b8d6e76f49ea5fb6753dbadccc20b3cb1
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-17 05:04:16 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ * a comma followed by a space, in ascending order
+ * Return: Always 0
+ */
+int main(void)
+{
+	int digit7, digit8;
+
+	for (digit7 = 0; digit7 < 10; digit7++)
+	{
+		for (digit8 = 0; digit8 < 10; digit8++)
+		{
+			putchar((digit7 % 10) + '0');
+			putchar((digit8 % 10) + '0');
+
+			if (digit7 == 9 && digit8 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
