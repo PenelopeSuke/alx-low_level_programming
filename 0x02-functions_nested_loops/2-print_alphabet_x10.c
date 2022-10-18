0d016e412fdf88c7a2190b3c9680088e3c3641bb
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-18 15:21:33 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,13 @@
  */
 void print_alphabet_x10(void)
 {
-int num = 0;
-char alpha;
+char alpha, num;
 for (num = 0; num <= 9; num++)
 {
 for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 _putchar(alpha);
 }
+_putchar('\n')
 }
-_putchar('\n');
 }
