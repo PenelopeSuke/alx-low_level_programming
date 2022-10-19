f279e1aa6ab405b1b51f5254019057f92d66cab3
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-19 01:55:21 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,6 +12,9 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
+			_putchar('0');
+		for (mult = 1; mult <= n; mult++)
+		{
 			_putchar(',');
 			_putchar(' ');
 
@@ -34,5 +37,6 @@ void print_times_table(int n)
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
+		}
 	}
 }
