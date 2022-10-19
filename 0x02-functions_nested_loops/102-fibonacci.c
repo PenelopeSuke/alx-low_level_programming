54b3d275d5563e4b398e83d19c8b174ab51624d4
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-19 02:12:22 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *  main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *  separated by a comma followed by a space.
+ *  Return: Always 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long fibo7 = 0, fibo8 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fibo7 + fibo8;
+		printf("%lu", sum);
+
+		fibo7 = fibo8;
+		fibo8 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
