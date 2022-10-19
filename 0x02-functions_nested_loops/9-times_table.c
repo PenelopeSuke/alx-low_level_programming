68edc48174bde0494a8864ec54a8f7279e847f88
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-18 20:34:04 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,25 +4,25 @@
  */
 void times_table(void)
 {
-	int num1, num2, num3;
+	int num, mult, prod;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
-		for (num2 = 1; num2 <= 9; num3++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
-			num3 = num1 * num2;
+			prod = num * mult;
 
-			if (num3 <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((num3 / 10) + '0');
+				_putchar((prod / 10) + '0');
 
-			_putchar((num3 % 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
