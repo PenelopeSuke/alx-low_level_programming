f5891a268ee19ea7934f6319ddb8a578a6b10a62
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-21 07:52:37 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of '_' to be printed
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
