7f835f9de3253b8882f3939596b4cd72d73912b7
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-21 06:03:47 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ *  Return: on success 1 else -1
+ */
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
