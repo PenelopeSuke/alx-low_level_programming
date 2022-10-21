16e1f2a02c056e16d318ec786dd21e20ba3f62bd
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-21 10:25:21 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - rints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	long int num = 612852475143;
+	int d;
+
+	for (d = (int) sqrt(num); d > 2; d++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
+	return (0);
+}
