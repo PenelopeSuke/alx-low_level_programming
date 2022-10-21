d52a87f5cababf105ce8e92c772e8d8f2b19cf4b
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-21 07:05:03 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,18 @@
  */
 void more_numbers(void)
 {
-	int x;
-	int y;
+	int x, y;
 
 	for (y = 0; y <= 9; y++)
 	{
 		for (x = 0; x <= 14; x++)
 		{
 			if (x > 9)
+			{
 				_putchar((x / 10) + '0');
+			}
+			_putchar((x % 10) + '0');
 		}
-		_putchar((x % 10) + '0');
+		_putchar ('\n');
 	}
-	_putchar ('\n');
 }
