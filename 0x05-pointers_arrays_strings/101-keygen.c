ea9bf24fc92d1258c98304819ef15d180efe1fa1
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-24 17:51:13 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 2772;
+	int a;
+
+	srand(time(0));
+	while (sum)
+	{
+		a = rand() % 100;
+		if (a <= 94)
+			a += 32;
+		else
+			continue;
+		if (sum - a == 0)
+		{
+			sum -= a;
+			printf("%c", a);
+		}
+		else if (sum - a - 32 > 0)
+		{
+			sum -= a;
+			printf("%c", a);
+		}
+	}
+	return (0);
+}
