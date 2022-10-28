9200e62f886bd8f4bf385465a0775b0e30726e70
PenelopeSuke <penelopesuke4@gmail.com>
2022-10-27 19:16:01 -0700

    t9

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,8 +10,24 @@ char *rot13(char *s)
 {
 	int a;
 	int b;
-	char c[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
-	char d[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char c[] = {'A', 'B', 'C', 'D', 'E', 'F',
+	            'G', 'H', 'I', 'J', 'K', 'L',
+		    'M', 'N', 'O', 'P', 'Q', 'R',
+		    'S', 'T', 'U', 'V', 'W', 'X',
+		    'Y', 'Z', 'a', 'b', 'c', 'd',
+		    'e', 'f', 'g', 'h', 'i', 'j',
+		    'k', 'l', 'm', 'n', 'o', 'p',
+		    'q', 'r', 's', 't', 'u', 'v',
+		    'w', 'x', 'y', 'z'};
+	char d[] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		    'T', 'U', 'V', 'W', 'X', 'Y',
+		    'Z', 'A', 'B', 'C', 'D', 'E',
+		    'F', 'G', 'H', 'I', 'J', 'K',
+		    'L', 'M', 'N', 'O', 'P', 'Q',
+		    'r', 's', 't', 'u', 'v', 'w',
+		    'r', 's', 't', 'u', 'v', 'w',
+		    'd', 'e', 'f', 'g', 'h', 'i',
+		    'j', 'k', 'l', 'm'};
 
 	while (s[a])
 	{
