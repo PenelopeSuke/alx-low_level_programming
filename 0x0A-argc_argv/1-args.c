bdd0c270b0337ac431007f691299d1b1c60ed121
PenelopeSuke <penelopesuke4@gmail.com>
2022-11-04 11:13:23 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (x = 0; *argv; x++, argv++)
+			;
+		printf("%d\n", x - 1);
+	}
+	return (0);
+}
