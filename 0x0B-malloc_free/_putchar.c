2e1f724d397ecb9f48b449175d6ae12445d62eec
PenelopeSuke <penelopesuke4@gmail.com>
2022-11-07 16:41:04 -0800

    Task 0

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"                                                                             
+#include <stdio.h>                                                                            
+/**
+ * _putchar - writes the character c to stdout
+ * Return: on success 1 else -1
+ */
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}	
