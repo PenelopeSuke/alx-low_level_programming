53fee6d4cb046813146d5a29309477c9e8f03253
PenelopeSuke <penelopesuke4@gmail.com>
2022-11-07 18:02:21 -0800

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ int _strlen(char *s)
  * *_strcat - concatenates two strings
  * @dest: input parameter string
  * @src: input parameter string
- * Retrn: dest
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
