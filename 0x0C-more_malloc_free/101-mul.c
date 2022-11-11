8d11b8b4aa5eae5f47daf674d82d4669c4083a89
PenelopeSuke <penelopesuke4@gmail.com>
2022-11-10 18:55:03 -0800

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,70 +2,33 @@
 #include <stdio.h>
 
 /**
- * _isdigit - tells if the string consists of digits
- * @argv: pointer to current item in argument
- * Return: return 0 if all digits, 1 if not all digits.
- */
-int _isdigit(char *argv)
-{
-	int a;
-
-	a = 0;
-	while (argv[a])
-	{
-		if (argv[a] >= '0' && argv[a] <= '9')
-			a++;
-		else
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * _atoi - converts a string of ascii digits to the values they represent
- * @s: pointer to the source string
- * Return: value of digits
- */
-int _atoi(char *s)
-{
-	int a, count;
-
-	a = count = 0;
-	while (s[a])
-	{
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			count *= 10;
-			count += (s[a] - '0');
-		}
-		a++;
-	}
-	return (count);
-}
-
-/**
  * main - main function call
  * @argc: argument count
- * @argv: array of arguments
- * Return: return 0 on success, 98 on failure
+ * @argv: 2D array of arguments
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int a;
+	unsigned long num;
+	int i, j;
 
-	malloc();
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	for (a = 1; a < argc; a++)
+	for (i = 1; i < argc; i++)
 	{
-		if (_isdigit(argv[a]))
+		for (j = 0; argc[i][j] != 0; j++)
 		{
-			printf("Error\n");
-			exit(98);
+			if (argc[i][j] > 57 || argc[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
 	}
+	num = atol(argc[1] * atol(argv[2]);
+	printf("%lu\n", num);
 	return (0);
 }
