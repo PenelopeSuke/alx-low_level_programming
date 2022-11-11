f1c35959b1f97ad2927def3e39888c416bfc88c7
PenelopeSuke <penelopesuke4@gmail.com>
2022-11-10 18:25:17 -0800

    Task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _isdigit - tells if the string consists of digits
+ * @argv: pointer to current item in argument
+ * Return: return 0 if all digits, 1 if not all digits.
+ */
+int _isdigit(char *argv)
+{
+	int a;
+
+	a = 0;
+	while (argv[a])
+	{
+		if (argv[a] >= '0' && argv[a] <= '9')
+			a++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * _atoi - converts a string of ascii digits to the values they represent
+ * @s: pointer to the source string
+ * Return: value of digits
+ */
+int _atoi(char *s)
+{
+	int a, count;
+
+	a = count = 0;
+	while (s[a])
+	{
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			count *= 10;
+			count += (s[a] - '0');
+		}
+		a++;
+	}
+	return (count);
+}
+
+/**
+ * main - main function call
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: return 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	malloc();
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (a = 1; a < argc; a++)
+	{
+		if (_isdigit(argv[a]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	return (0);
+}
