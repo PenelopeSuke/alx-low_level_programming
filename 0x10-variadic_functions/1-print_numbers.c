e2b5dbe876bbfc72a86c2a5ab7e4e24cb7906c2f
PenelopeSuke <penelopesuke4@gmail.com>
2022-11-17 19:03:37 -0800

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ * @n: number of arguments
+ * @separator: character separator of numbers
+ * Return: 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int a;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	va_start(num, n);
+
+	for (a = 0; a < n; a++)
+	{
+		printf("%d", va_arg(num, int));
+		if (n == a + 1)
+		{
+			break;
+		}
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(numbers);
+}
