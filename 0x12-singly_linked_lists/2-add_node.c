deed1f29160a3a640a8bc100dcec8fe425af8ea1
PenelopeSuke <penelopesuke4@gmail.com>
2022-11-24 17:05:52 -0800

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * *add_node - adds a new node at the beginning
@@ -12,7 +13,6 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node;
 	char *dup;
-	int len;
 
 	node = malloc(sizeof(list_t));
 	if (node == NULL)
