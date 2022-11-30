591e15e14377bd13ec6948311f295e95b7bf28ff
PenelopeSuke <penelopesuke4@gmail.com>
2022-11-29 20:29:41 -0800

    Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Locates a given node of
+ * a listint_t linked list
+ * @index: The index of the node to locate
+ * Return: NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int a;
+
+	for (a = 0; a < index; a++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
