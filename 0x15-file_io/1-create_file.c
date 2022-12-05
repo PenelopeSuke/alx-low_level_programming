f849399dfaed37f1468a3039bfbf06de73981d85
PenelopeSuke <penelopesuke4@gmail.com>
2022-12-05 09:50:24 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	while (text_content[i] != '\0')
+	while (text_content[a] != '\0')
 	{
 		a++;
 	}
