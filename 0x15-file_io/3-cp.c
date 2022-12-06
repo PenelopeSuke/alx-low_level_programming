e5219d8570af61c28fb4149e4b1046e06ce7527f
PenelopeSuke <penelopesuke4@gmail.com>
2022-12-05 22:43:10 -0800

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	char *buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file1_from file1_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file1_from = open(argv[1], O_RDONLY);
 	if (file1_from == -1)
 	{
